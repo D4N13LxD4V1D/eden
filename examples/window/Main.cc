6542bfc29813cb2b0f6c50d84a03008c84cf696a
Daniel David N. Bador <danieldavidbador@yahoo.com>
2025-01-21 00:13:51 +0800

    remove log

--- examples/window/Main.cc
+++ examples/window/Main.cc
@@ -1,7 +1,6 @@
 import eden;
 
 int main(int argc, const char *argv[]) {
-  eden::log("HI!");
   eden::Window window;
 
   return 0;
--- src/Eden.cc
+++ src/Eden.cc
@@ -2,6 +2,5 @@ export module eden;
 
 export import :ecs;
 export import :event_queue;
-export import :log;
 export import :renderer;
 export import :window;
